44377bce98148947a053e956093a668a64aad5e0
Mikhail Polyakov PyCharm <mikhapol@bk.ru>
2023-05-13 21:13:47 +0300

    Выполнение HW-1

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,14 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+
+from src.stack import Stack
+
+
+class TestStack(unittest.TestCase):
+    def test_push(self):
+        test_stack_push = Stack()
+        test_stack_push.push("One")
+        self.assertEqual(test_stack_push.top.data, "One")
+        test_stack_push.push("Two")
+        self.assertEqual(test_stack_push.top.data, "Two")
+        self.assertEqual(test_stack_push.top.next_node.data, "One")
